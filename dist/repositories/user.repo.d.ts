2f7dcd941981e0360833d39287469b3b4caf0ab7
Mathiyaseelan Ruckshanth <Ruckshanth.Mathiyaseelan@teceze.com>
2026-03-31 11:31:20 +0530

    chore: remove dist from tracking, add to .gitignore

--- dist/repositories/user.repo.d.ts
+++
@@ -1,9 +0,0 @@
-import { NewPermission, Permission, Role } from "../schema/index.js";
-export declare class UserRepository {
-    createPermission(permissionData: NewPermission): Promise<Permission>;
-    getPermission(noteId: string, userId: string): Promise<Permission | null>;
-    updatePermissionRole(noteId: string, userId: string, role: Role): Promise<Permission | null>;
-    deletePermission(noteId: string, userId: string): Promise<boolean>;
-}
-export declare const userRepository: UserRepository;
-//# sourceMappingURL=user.repo.d.ts.map
